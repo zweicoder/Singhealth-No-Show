32c59e58e822b186e0e7a9c867c1fa5c57adc0a7
yunkez <zhouyunke@zhou.local>
2016-03-22 19:12:09 +0800

    update csv data file

--- aid-match.py
+++ aid-match.py
@@ -22,31 +22,23 @@ def read_matching():
             raw = line.strip().split(',')
             hashmap[raw[0]] = raw[1]
     return hashmap
-
-def write_column():
+   
+def update_file():
     files = ['csv/quarter%s.csv' % i for i in range(1,5,1)]
+    newfiles = ['csv/quarter%s_new.csv' % i for i in range(1,5,1)]
     hashmap = read_matching()
-    with open('csv/pid.csv','w') as f:
-        for input_file in files:
-            with open(input_file,'rb') as f2:
-                for line in f2:
-                    raw = line.strip().split(',')
-                    aid = raw[c_cols["eHIntS Appointment Id"]]
-                    if aid in hashmap:
-                        f.write(aid + ',' + hashmap[aid] + ',' + raw[c_cols["Appointment Date"]] + ',' + raw[c_cols["Visit Status Code"]] + "\n")
-    
-def get_wrongid():
-    write_column()
     data = []
-    data_new = []
-    with open('csv/pid.csv','rb') as f:
-        for line in f:
-            raw = line.strip().split(',')
-            data.append(raw)
+    for input_file in files:
+        with open(input_file,'rb') as f:
+            for line in f:
+                raw = line.strip().split(',')
+                aid = raw[c_cols["eHIntS Appointment Id"]]
+                if aid in hashmap:
+                    data.append([aid, hashmap[aid] , raw[c_cols["Appointment Date"]] , raw[c_cols["Visit Status Code"]]])    
     data = sorted(data, key=operator.itemgetter(1, 2))
-    print len(data)
     patient = ""
     first = -1
+    wrong_aid = set()
     for i in range(len(data)):
         if data[i][1]!=patient:
             patient=data[i][1]
@@ -54,9 +46,20 @@ def get_wrongid():
         if data[i][3] == "NS":
             for j in range(first,i):
                 if data[j][2] == data[i][2] and data[j][3] == "A":
-                    data_new.append(data[j][0])
-    with open('csv/pid_new.csv','w') as f:
-        for d in data_new:
-            f.write(d+"\n")
+                    wrong_aid.add(data[i][0])
+    
+    for i in range(len(files)):
+        with open(files[i],'rb') as f1:
+            with open(newfiles[i],'w') as f2:
+                for line in f1:
+                    raw = line.strip()
+                    row = raw.split(',')
+                    aid = row[c_cols["eHIntS Appointment Id"]]
+                    if aid not in wrong_aid:
+                        f2.write(raw+"\n")
+                    else:
+                        row[c_cols["Visit Status Code"]] = "A"
+                        f2.write(','.join(row)+"\n")
+                    
 
-get_wrongid()
+update_file()
