a4efc898d4f96caa6f9e21b5a4421534dbd11b77
yunkez <zhouyunke@zhou.local>
2016-03-22 13:26:54 +0800

    Find wrong appointment id

--- aid-match.py
+++ aid-match.py
@@ -1,3 +1,4 @@
+import operator
 
 cols = ["Institution Description", "Appointment Year", "Appointment Quarter", "Appointment Month",
         "Appointment Week Name", "Appointment Date", "Appointment Time", "eHIntS Appointment Id",
@@ -25,7 +26,7 @@ def read_matching():
 def write_column():
     files = ['csv/quarter%s.csv' % i for i in range(1,5,1)]
     hashmap = read_matching()
-    with open('pid.csv','w') as f:
+    with open('csv/pid.csv','w') as f:
         for input_file in files:
             with open(input_file,'rb') as f2:
                 for line in f2:
@@ -33,8 +34,29 @@ def write_column():
                     aid = raw[c_cols["eHIntS Appointment Id"]]
                     if aid in hashmap:
                         f.write(aid + ',' + hashmap[aid] + ',' + raw[c_cols["Appointment Date"]] + ',' + raw[c_cols["Visit Status Code"]] + "\n")
+    
+def get_wrongid():
+    write_column()
+    data = []
+    data_new = []
+    with open('csv/pid.csv','rb') as f:
+        for line in f:
+            raw = line.strip().split(',')
+            data.append(raw)
+    data = sorted(data, key=operator.itemgetter(1, 2))
+    print len(data)
+    patient = ""
+    first = -1
+    for i in range(len(data)):
+        if data[i][1]!=patient:
+            patient=data[i][1]
+            first = i
+        if data[i][3] == "NS":
+            for j in range(first,i):
+                if data[j][2] == data[i][2] and data[j][3] == "A":
+                    data_new.append(data[j][0])
+    with open('csv/pid_new.csv','w') as f:
+        for d in data_new:
+            f.write(d+"\n")
 
-
-
-write_column()
-
+get_wrongid()
