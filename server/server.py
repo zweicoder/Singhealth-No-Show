bd340312532bf5c571059d2f9918e77de0c3fe13
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-06-11 14:14:17 +0800

    Condense reduction logic and add dumpResult feature

--- server/server.py
+++ server/server.py
@@ -19,37 +19,56 @@ threshold = 0.75 #  or 0.75
 @app.route('/predict', methods=['GET'])
 def getPredictions():
     with open(filename) as f:
-        preds =  predictor.predict()
-        lines = f.read().split('\n')[:-1]
-        lines = [line.split(',') for line in lines]
+        lines = f.read().split('\n')
+        preds = []
+        for pred, line in itertools.izip(predictor.predict(), lines):
+            # Case of empty string from empty \n
+            if line and pred > threshold:
+                line = line.split(',')
+                specialty, id, date, patientName, phoneNum, resourceName  = line[12], line[4], line[2], line[-3], line[-2], line[-1]
+                preds.append({
+                    'specialty':specialty, 
+                    'patientName': patientName,
+                    'phoneNum': phoneNum,
+                    'resourceName': resourceName,
+                    'score':'%s'% pred, 
+                    'date': date,
+                    'id':id 
+                    })
+        
         # Specialty and id row
-        specialty, id, date = zip(*((line[12], line[4], line[2]) for line in lines))
         day_to_num = dict(zip(list(day_name), range(7)))
-        preds = [{
-            'specialty':specialty[i], 
-            'score':'%s'%preds[i], 
-            'date': date[i],
-            'id':id[i] 
-        # Filter all that is less than the threshold
-        } for i in xrange(len(preds)) if preds[i] > threshold]
         
         sorted(preds, key=lambda p:p['date'])
         preds = [{
-        'date':key,
-        'dayOfWeek': day_to_num[datetime.strptime(key, '%d/%m/%y').strftime('%A')],
-        'predictions': [
-            {
-            'score': item['score'], 
-            'id':item['id'],
-            'specialty': item['specialty']
-            } for item in group
-            ]}
-        for key, group in itertools.groupby(preds, lambda item: item['date'])
+            'date':key,
+            'dayOfWeek': day_to_num[datetime.strptime(key, '%d/%m/%y').strftime('%A')],
+            'predictions': [item for item in group]
+            } for key, group in itertools.groupby(preds, lambda item: item['date'])
         ] 
-
+        dumpResults(preds)
         return jsonify(data=preds)
 
     return 'ERROR'
+
+def dumpResults(results):
+    import os
+    directory = 'results'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(directory+'/results.csv','w') as f:
+        write = lambda s: f.write(s + '\n')
+        write(','.join([
+            'Date','Specialty','Case ID','Patient Name',
+            'Phone Number', 'Resource Name', 'Probability'
+            ]))
+        for result in results:
+            for pred in result['predictions']:
+                write(','.join([
+                    pred['date'], pred['specialty'], pred['id'], pred['patientName'], 
+                    pred['phoneNum'], pred['resourceName'], pred['score']
+                    ]))
+                
     
 if __name__ == '__main__':
     app.run()
